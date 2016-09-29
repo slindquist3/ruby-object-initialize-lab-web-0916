d24db4510e51d86f9efbcb9dc57deb0a0cdd5abd
slindquist3 <slindquist3@gmail.com>
2016-09-29 11:21:49 -0400

    Use #initialize

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  end
